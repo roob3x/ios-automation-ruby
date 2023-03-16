66f574071daa174080dac58a6b76a0a1cc10cccc
Roberto Filho <roberto.siilva@hotmail.com>
2023-03-16 10:42:17 -0300

    remocao de linhas comentadas

--- features/core/pages/home_page.rb
+++ features/core/pages/home_page.rb
@@ -17,26 +17,6 @@ class HomePage
       click @mappings['add_btn']
   end
 
-  #def send_keys_marca text
-  #    fill_in @mappings['marca_input'], text
-  #end
-
-  #def send_keys_modelo text
-  #    fill_in @mappings['modelo_input'], text
-  #end
-
-  #def send_keys_versao text
-  #    fill_in @mappings['versao_input'], text
-  #end
-
-  #def send_keys_ano text
-  #    fill_in @mappings['ano_input'], text
-  #end
-
-  #def click_resgister_btn
-  #    click @mappings['cadastrar_btn']
-  #end
-
   def exclude_car
     swiper_element_direction @mappings['tcross_menu'], 'left'
     click @mappings['delete_menu']
